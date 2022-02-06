934c9449f08a85779f3cb246025cd73c3756e01e
Moncef-Tighi <monceftighiouart@gmail.com>
2022-02-06 21:54:02 +0100

    Créer un wrapper component pour enlever de la dupplication de code

--- learning React/react-starting-app/src/components/Expense.js
+++ learning React/react-starting-app/src/components/Expense.js
@@ -1,15 +1,16 @@
 import './Expense.css'
 import ExpenseDate from './ExpenseDate';
+import Card from "./Card";
 
 function Expense(props) {
     return(
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <p className='expense-item__price'>${props.amount}</p>
             </div> 
-        </div> 
+        </Card> 
     )
 };
 
