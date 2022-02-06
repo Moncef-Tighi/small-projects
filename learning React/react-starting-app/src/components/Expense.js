eab8bdb7df0e26abbde5c6c8007d423a3454a509
Moncef-Tighi <monceftighiouart@gmail.com>
2022-02-06 21:25:29 +0100

    Modification de l'Expense pour la rendre plus dynamique et ajout d'un component date

--- learning React/react-starting-app/src/components/Expense.js
+++ learning React/react-starting-app/src/components/Expense.js
@@ -1,9 +1,10 @@
 import './Expense.css'
+import ExpenseDate from './ExpenseDate';
 
 function Expense(props) {
     return(
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <p className='expense-item__price'>${props.amount}</p>
