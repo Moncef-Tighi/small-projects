42e16e5c21a5795f00551ef7f4934ce9726c461c
Moncef-Tighi <monceftighiouart@gmail.com>
2022-02-10 19:32:13 +0100

    Gestion de la state avec un contexte

--- learning React/react-authentication-system/src/App.js
+++ learning React/react-authentication-system/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './state/authContext';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -26,13 +27,13 @@ function App() {
   } , [isLoggedIn]); 
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+      <AuthContext.Provider  value={ {isLoggedIn : isLoggedIn, onLogout: logoutHandler} }>
+        <MainHeader onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
   );
 }
 
--- learning React/react-authentication-system/src/components/MainHeader/Navigation.js
+++ learning React/react-authentication-system/src/components/MainHeader/Navigation.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import AuthContext from '../../state/authContext';
 
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
+  const contextValue= useContext(AuthContext);
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {contextValue.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {contextValue.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {contextValue.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={contextValue.onLogout}>Logout</button>
           </li>
         )}
       </ul>
