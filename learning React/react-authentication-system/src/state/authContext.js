f17b0eba4fc55bc080808099072187ed096870ac
Moncef-Tighi <monceftighiouart@gmail.com>
2022-02-10 20:04:16 +0100

    small bug fix

--- learning React/react-authentication-system/src/state/authContext.js
+++ learning React/react-authentication-system/src/state/authContext.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const AuthContext =React.createContext({
     isLoggedIn : false,
+    onLogout: ()=>{} //la fonction est défini dans le Provider et non pas par défaut
 });
 
 
-export default AuthContext; 
+export default AuthContext;  
