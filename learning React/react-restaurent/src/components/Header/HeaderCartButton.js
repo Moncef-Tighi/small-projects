69abfe8e2fdd9eca840a47be1697cd1c8c15547b
Moncef-Tighi <monceftighiouart@gmail.com>
2022-02-15 22:42:02 +0100

    Amélioration de la lisibilité avec une animation dynamique

--- learning React/react-restaurent/src/components/Header/HeaderCartButton.js
+++ learning React/react-restaurent/src/components/Header/HeaderCartButton.js
@@ -1,14 +1,22 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../CartIcon';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartState from '../../state/cartState';
 
 
 const HeaderCartButton = function(props) {
     const data = useContext(CartState);
+    const [buttonAnimation, setButtonAnimation]= useState(false);
+    const animationClasses = `${classes.button} ${buttonAnimation ? classes.bump : ''}`
+    useEffect(()=>{
+        if (data.items.length) setButtonAnimation(true);
+        return setTimeout( ()=> {
+            setButtonAnimation(false);
+        }, 300 )
+    }, [data.items])
     return (
         <>
-            <button className={classes.button} onClick={props.onClick}>
+            <button className={animationClasses} onClick={props.onClick}>
                 <span className={classes.icon}><CartIcon/></span>
                 <span>Votre commande</span>
                 <span className={classes.badge}>
