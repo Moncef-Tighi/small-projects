2c377b919cefb2b49af53e90cd741abf09281792
Moncef-Tighi <monceftighiouart@gmail.com>
2022-02-18 21:35:29 +0100

    Complémentation de l'application en la connectant avec une DB Firebase

--- learning React/react-movies/src/App.js
+++ learning React/react-movies/src/App.js
@@ -22,20 +22,39 @@ function App() {
     setError(null);
     try {
       const data = await fetchMovies();
-      const moviesData = data.results.map( (movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText : movieData.opening_crawl,
-          releaseDate : movieData.release_date
-        }
-      });
+      const moviesData = [];
+      for (const key in data) {
+        moviesData.push({
+          id : key,
+          title : data[key].title,
+          openingText : data[key].openingText,
+          releaseDate : data[key].releaseDate
+        })
+      }
+      // const moviesData = data.results.map( (movieData) => {
+      //   return {
+      //     id: movieData.name,
+      //     title: movieData.title,
+      //     openingText : movieData.openingText,
+      //     releaseDate : movieData.releaseDate
+      //   }
+      // });
       setMovies(moviesData);
       setIsLoading(false);
     } catch(e) {
       setError(e);
     }
   } 
+
+  async function addMovieHandler(movie) {
+      const request = await fetch("https://react-movies-33435-default-rtdb.europe-west1.firebasedatabase.app/movies.json", {
+        method: 'POST',
+        body : JSON.stringify(movie),
+        headers: {
+          'Content-Type' : 'application/json'
+        }
+      })       
+  }
 
   let page = <h2>Aucun film n'a été chargé</h2>
   if (movies.length>0) {
@@ -51,7 +70,7 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <AddMovie/>
+        <AddMovie onAddMovie={addMovieHandler}/>
       </section>
       <section>
         <button onClick={loadData}>Afficher les films</button>
