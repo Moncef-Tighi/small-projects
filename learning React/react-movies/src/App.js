3e5410c65bd2e3feebbea077f95450fde120b36c
Moncef-Tighi <monceftighiouart@gmail.com>
2022-02-18 19:52:20 +0100

    Création de l'application de films

---
+++ learning React/react-movies/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+const fetchMovies= async function() {
+  const request = await fetch("https://swapi.dev/api/films");
+  return await request.json();
+}
+
+
+function App() {
+
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
+
+  return (
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
