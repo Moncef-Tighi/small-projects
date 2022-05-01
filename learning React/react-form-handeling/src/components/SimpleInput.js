beab6667c16ad834ab31e26de0e696e65ffeee36
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-01 15:04:56 +0100

    Validation avec un seul input

--- learning React/react-form-handeling/src/components/SimpleInput.js
+++ learning React/react-form-handeling/src/components/SimpleInput.js
@@ -1,24 +1,36 @@
 import {useRef, useState} from 'react';
-import css from './SimpleInput.module.css';
+
+const validate= function(value) {
+  if (value.current.value.trim()==='') return true
+  return false;
+}
 
 const SimpleInput = (props) => {
   
   const name = useRef();
   const [error, setError]=useState(false);
-  
+
+
+  const exitingInput = event=> {
+    event.preventDefault();
+    setError(validate(name));
+  }
   const formSubmission = event => {
     event.preventDefault();
-    if (!name.current.value) return setError(true)
-    setError(false);
+    setError(validate(name));
     console.log(name.current.value);
+  }
 
+  const resetValidation = event => {
+    if (event.target.value.trim()!=='') setError(false);
   }
 
   return (
     <form onSubmit={formSubmission}>
-      <div className='form-control'>
+      <div className={error ? "form-control invalid" : 'form-control'}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' ref={name} className={error ? css.erreur : ""}/>
+        <input type='text' id='name' ref={name} onBlur={exitingInput} onChange={resetValidation}/>
+        {error ? <p className='error-text'>Le nom ne peut pas être vide</p> : ''}
       </div>
       <div className="form-actions">
         <button>Submit</button>
