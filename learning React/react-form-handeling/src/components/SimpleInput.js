600c0be7bc9344116c934f43df27a84866b70507
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-01 14:08:48 +0100

    erreur feedback on form

--- learning React/react-form-handeling/src/components/SimpleInput.js
+++ learning React/react-form-handeling/src/components/SimpleInput.js
@@ -1,9 +1,24 @@
+import {useRef, useState} from 'react';
+import css from './SimpleInput.module.css';
+
 const SimpleInput = (props) => {
+  
+  const name = useRef();
+  const [error, setError]=useState(false);
+  
+  const formSubmission = event => {
+    event.preventDefault();
+    if (!name.current.value) return setError(true)
+    setError(false);
+    console.log(name.current.value);
+
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmission}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' ref={name} className={error ? css.erreur : ""}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
