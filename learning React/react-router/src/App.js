dee27859ec5ceabbbae71e045bcd79df3a635ead
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-03 19:32:21 +0100

    Ajout du lazy loading sur les pages du siteweb

--- learning React/react-router/src/App.js
+++ learning React/react-router/src/App.js
@@ -1,22 +1,34 @@
+import React, {Suspense} from "react";
 import { Routes, Route } from "react-router-dom";
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import QuoteDetail from "./pages/QuoteDetail";
-import Comments from './components/comments/Comments';
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+// import AllQuotes from "./pages/AllQuotes";
+// import NewQuote from "./pages/NewQuote";
+// import QuoteDetail from "./pages/QuoteDetail";
+// import Comments from './components/comments/Comments';
+// import NotFound from "./pages/NotFound";
+
+const AllQuotes = React.lazy(()=> import("./pages/AllQuotes"))
+const NewQuote = React.lazy(()=> import( "./pages/NewQuote"))
+const QuoteDetail = React.lazy(()=> import("./pages/QuoteDetail"))
+const Comments = React.lazy(()=> import('./components/comments/Comments'))
+const NotFound = React.lazy(()=> import("./pages/NotFound"))
+
 function App() {
 
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<AllQuotes/>} />
-        <Route path="new" element={<NewQuote/>} />
-        <Route path="detail/:quoteId" element={<QuoteDetail/>}>
-          <Route path="comments" element={<Comments />} />
-        </Route>
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
+      <Suspense fallback={<LoadingSpinner/>}>
+        <Routes>
+          <Route path="/" element={<AllQuotes/>} />
+          <Route path="new" element={<NewQuote/>} />
+          <Route path="detail/:quoteId" element={<QuoteDetail/>}>
+            <Route path="comments" element={<Comments />} />
+          </Route>
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
