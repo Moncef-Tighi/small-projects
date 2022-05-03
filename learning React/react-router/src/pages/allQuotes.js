4f7c4e68401d0546ec7ce0b4376183de7df58aac
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-03 16:39:04 +0100

    Ajout des citations

--- learning React/react-router/src/pages/allQuotes.js
+++ learning React/react-router/src/pages/allQuotes.js
@@ -1,7 +1,20 @@
+import QuoteList from '../components/quotes/QuoteList'
+
+const dummyquotes = [
+    {id: "1", author: 'aubgiube', text : 'oinoibiugbae'},
+    {id: "1", author: 'htt', text : 'enhe nethdgbet eth'},
+    {id: "1", author: ' zrz zgz', text : ' ethtujryjyilk  grhryn  trhrdf  deth'},
+    {id: "1", author: 'aubgzrgzrgzgiube', text : 'oinoibiugbae'},
+    {id: "1", author: 'r z zbgiube', text : 'ez zgfz zfvzrbz z zr'},
+    {id: "1", author: 'poitnhoropn', text : 'zrgzrgzdcv sgbgregr'},
+    {id: "1", author: 'gzrg', text : 'gzrgezrgzrgzgzr'},
+    {id: "1", author: 'pàblo', text : 'gzrgz'}
+]
+
 
 const AllQuotes = () => {
     return (
-    <h1>All Quotes Page</h1>
+        <QuoteList quotes={dummyquotes}/>
     )
   };
   
--- learning React/react-router/src/pages/newQuote.js
+++ learning React/react-router/src/pages/newQuote.js
@@ -1,5 +1,10 @@
+import QuoteForm from '../components/quotes/QuoteForm.js'
+
 const NewQuote = () => {
-    return <h1>New Quote Page</h1>
+    const addQuote= quoteData=>{
+        console.log(quoteData);
+    }
+    return <QuoteForm onAddQuote={addQuote} />
   };
   
   export default NewQuote;
