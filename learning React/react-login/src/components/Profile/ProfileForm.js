38f9971b336a032683295635d4b604854033a44a
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-04 17:26:35 +0100

    Création du profile d'authentification

--- learning React/react-login/src/components/Profile/ProfileForm.js
+++ learning React/react-login/src/components/Profile/ProfileForm.js
@@ -1,11 +1,34 @@
 import classes from './ProfileForm.module.css';
+import { useRef } from 'react';
+import { useContext } from 'react';
+import AuthContext from '../store/authContext';
 
 const ProfileForm = () => {
+  const passwordInput = useRef();
+  const {token}= useContext(AuthContext);
+
+  const submitHandeler = (event) => {
+    event.preventDefault();
+
+    const password = passwordInput.current.value;
+
+    fetch('endpoint-api', {
+      method : "POST",
+      headers : {
+        'Content-Type' : 'application/json',
+        'Authorization' : `Bearer ${token}`
+      },
+      body : {
+        password,
+        returnSecureToken : true
+      }
+    })
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandeler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength='3' ref={passwordInput} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
