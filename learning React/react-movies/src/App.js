0252c44ea49fe4d46b4ef750a43efe8272ece364
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-05 21:35:56 +0100

    Ajout d'une nouvelle liste

--- learning React/react-movies/src/App.js
+++ learning React/react-movies/src/App.js
@@ -10,6 +10,30 @@ const fetchMovies= async function() {
   return await request.json();
 }
 
+const movies = [
+  {          
+    key: "a",
+    title: "Star Wars",
+    releaseDate: 2001,
+    description : "The story of the original trilogy focuses on Luke Skywalker's quest to become a Jedi, his struggle with the evil Imperial agent Darth Vader, and the struggle of the Rebel Alliance to free the galaxy from the clutches of the Galactic Empire.",
+    image: "starwar.jpg"
+  },
+  {          
+    key: "b",
+    title: "Avatar",
+    releaseDate: 2010,
+    description : "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    image: "avatar.jpg"
+  },
+  {          
+    key: "c",
+    title: "Venom",
+    description : "A failed reporter is bonded to an alien entity, one of many symbiotes who have invaded Earth. But the being takes a liking to Earth and decides to protect it. ",
+    releaseDate: 2018,
+    image: "venom.jpg"
+  },
+]
+
 
 function App() {
 
@@ -70,13 +94,13 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <AddMovie onAddMovie={addMovieHandler}/>
+        <AddMovie/>
       </section>
       <section>
-        <button onClick={loadData}>Afficher les films</button>
+        <button>Afficher les films</button>
       </section>
       <section>
-        {page}
+        <MoviesList movies={movies}/>
       </section>
     </React.Fragment>
   );
