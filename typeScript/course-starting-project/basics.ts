3746f7bec4ab9eeace590740ff189ba05cb52f7f
Moncef-Tighi <monceftighiouart@gmail.com>
2022-12-31 19:31:24 +0100

    TypeScript basic types

---
+++ typeScript/course-starting-project/basics.ts
@@ -0,0 +1,12 @@
+
+// const add = function(...numbers) {
+//     return numbers.reduce((total, number)=> total+number ,0)
+// }
+
+const add = function (n1 : number, n2 : number) {
+  return n1 + n2
+}
+
+const result = add(10, "15")
+
+console.log(result)
